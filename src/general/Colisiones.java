4ffc77d2579976df101f415ac22ec918fd512da1
Ezequiel <ezequielmaccarini@gmail.com>
2018-10-09 14:54:08 -0300

    a ver

--- src/general/Colisiones.java
+++ src/general/Colisiones.java
@@ -2,4 +2,10 @@ package general;
 
 public class Colisiones {
 
+	public static void comprobarColision() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- src/general/Nivel.java
+++ src/general/Nivel.java
@@ -5,37 +5,46 @@ public class Nivel {
 	private Double velocidad;
 	private Integer[] numeroDeEnemigos;
 	private Base[] bases;
+
 	public Nivel(Integer nivelActual) {
 		// TODO Auto-generated constructor stub
 	}
+
 	public Nivel() {
 		// TODO Auto-generated constructor stub
 	}
-	public void loopDelNivel(){
-		while(Oleada.hayEnemigos()) 			//IMPLEMENTAR
-			Thread.sleep(1000/60);
-        	this.Actualizar();
-        	Colisiones.comprobarColision(); 
-        	
+
+	public void loopDelNivel() {
+		while (Oleada.hayEnemigos()) // IMPLEMENTAR
+			try{
+				Thread.sleep(1000 / 60);
+			}
+		catch(Exception e){
+			System.err.println(e);
+		}
+		this.Actualizar();
+		Colisiones.comprobarColision();
 	}
-	
-	
+
 	private void Actualizar() {
-		
-		
+
 	}
+
 	public Base[] getBases() {
 		return bases;
 	}
+
 	public void setBases(Base[] bases) {
 		this.bases = bases;
 	}
+
 	public boolean getPerdio() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	public void jugar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
