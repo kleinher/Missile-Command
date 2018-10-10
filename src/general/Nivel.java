33ed092353549dd19d595793bab21e7429632a67
Hernan <hernan.k97@gmail.com>
2018-10-10 18:32:23 -0300

    Cambios en el funcionamiento del loop del nivel

--- src/general/Nivel.java
+++ src/general/Nivel.java
@@ -5,52 +5,52 @@ import java.util.LinkedList;
 import enemigos.*;
 
 public class Nivel {
-	private Integer NumeroDeNivel;
-	private Long velocidad;
+	//Variables de juego
+	private Long Velocidad;
+	private boolean Perdio;
+	
+	//Variables enemigas
 	private LinkedList<Enemigo> EnemigosEnPantalla;
+	private LinkedList<Enemigo> Enemigos;
+	
+	//Variables aliadas
 	private LinkedList<MisilAntibalistico> ListaMisilesAntibalisticos;
-	private Ciudad ciudades[];
-	private Base bases[];
+	private Ciudad Ciudades[];
+	private Base Bases[];
 	
 	
-	public Nivel(Integer nivelActual) { /* limpia pantalla,reponer misiles, reconstruye bases */
-		LinkedList Aliados = new LinkedList();
+	public Nivel() { /* limpia pantalla,reponer misiles, reconstruye bases */
 		Ciudad ciudades[] = new Ciudad[7];
-		Base bases[]= new Base[2];
+		Base bases[]= new Base[4];
 		
 		//Crea la lista de enemigos del nivel
 		LinkedList<Enemigo> Enemigos = new LinkedList<Enemigo>(); 
-		Oleada.CrearListaDeOleadasPorNivel(this.NumeroDeNivel, Enemigos);
+		Oleada.CrearListaDeOleadasPorNivel(Juego.getNivelActual(), Enemigos);
+		
 		//Instancia las nueve ciudades
-		Ciudad.InstanciarCuidades(ciudades);
+		Ciudad.InstanciarCiudades(ciudades);
 		
 		//Instancia las tres bases
 		Base.InstanciarBases(bases);
 	}
 
-	public Nivel() {
-
-	}
 	/*LOOP NIVEL
 	 * En este método se va a iterar todo el nivel, básicamente es el método principal del juego
 	 */
 	public void loopDelNivel() 
 		throws InterruptedException{
-		Integer segundos=new Integer(0);
-		
-		// Mientras hayan queden ciudades en pie
-		while (Ciudad.hayCiudades(ciudades))
+		// Mientras hayan queden ciudades en pie. Mientras hayan enemigos
+		while (!Enemigos.isEmpty())
 		{
-			// Mientras hayan enemigos
-			while (!Enemigos.isEmpty())
-			{
 			this.actualizarPosiciones();
 			Colisiones.comprobarColision(this);
 			//dibujar();
 			Thread.sleep(1000/60);
 			//pausa entre una oleada y la siguiente
-			}
 		}
+	    if(!Ciudad.hayCiudades(Ciudades)) {
+	    	this.Perdio = true;
+	    }
 		PuntajeJugador.actualizarTablaDePuntajes(); // PARAMETROS
 		// MostrarPuntaje (ParteGrafica)
 
@@ -74,6 +74,10 @@ public class Nivel {
 			MisilAntibalistico misil = i.next();
 			misil.mover();
 		}
+	}
+
+	public boolean getPerdio() {
+		return this.Perdio;
 	}
 
 }
