087d1ded18139ff04a7babe7e6c92019a9716275
Hernan <hernan.k97@gmail.com>
2018-10-10 00:11:01 -0300

    Mover de enemigos

--- src/enemigos/Enemigo.java
+++ src/enemigos/Enemigo.java
@@ -1,6 +1,23 @@
 package enemigos;
-
+import general.Posicion;
 public abstract class Enemigo {
+	protected Posicion PosicionInicial;
+	protected Posicion PosicionObjetivo;
+	
 	public abstract void mover();
+	
+	public Posicion getPosicionInicial() {
+		return PosicionInicial;
+	}
+	public void setPosicionInicial(Posicion posicionInicial) {
+		PosicionInicial = posicionInicial;
+	}
+	public Posicion getPosicionObjetivo() {
+		return PosicionObjetivo;
+	}
+	public void setPosicionObjetivo(Posicion posicionObjetivo) {
+		PosicionObjetivo = posicionObjetivo;
+	}
+	
 		
 }
--- src/enemigos/MisilBalistico.java
+++ src/enemigos/MisilBalistico.java
@@ -1,7 +1,14 @@
 package enemigos;
 
 public class MisilBalistico extends Misiles {
-	public void mover() {
-		
+	
+	/*
+	 *MOVER 
+	 *Este método va a calcular el desplazamiento del misil balistico interplanetario(Enemigo)
+	 *
+	 */
+	public void mover(){
+		float pendiente;
+		float distancia;
 	}
 }
