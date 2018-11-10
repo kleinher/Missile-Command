973fc1a0c2ba080d92be559652a047df3d78ca9a
ezequiel <ezequiel@ezequiel-Rev-1-0>
2018-11-10 19:11:31 -0300

    va queriendo

---
+++ src/controlador/Main.java
@@ -0,0 +1,12 @@
+package controlador;
+import java.awt.Toolkit;
+
+import vista.MenuPrincipal;
+
+public class Main {
+	public static void main(String[] args) {
+		MenuPrincipal menu=new MenuPrincipal();
+		
+	}
+
+}
