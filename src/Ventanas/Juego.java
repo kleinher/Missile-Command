1ec59ca1a59d687eb1c2afae5dec61d066924363
Hernan <hernan.k97@gmail.com>
2018-11-15 02:19:21 -0300

    Agregado el disparar misil con teclas
    Se rompe cuando quiero recuperar la posicion del mouse dentro de la
    pantalla

---
+++ src/Ventanas/Juego.java
@@ -0,0 +1,81 @@
+package Ventanas;
+
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+
+import controlador.AdapterDeMiraMouse;
+import controlador.AdapterDeMiraTeclado;
+import modelo.gestores.GestorDeNivel;
+import modelo.gestores.Pantalla;
+
+public class Juego extends JFrame {
+	//Variable de el JPanel "pantalla"
+	public Pantalla pantalla;
+	
+	public void Controlar() {
+		Timer timer;
+		timer = new Timer();
+		//Variable de el JPanel "pantalla"
+		Pantalla pantalla = new Pantalla();
+		add(pantalla);
+		pantalla.addMouseListener(new AdapterDeMiraMouse());
+		pantalla.addKeyListener(new AdapterDeMiraTeclado());
+		//configuracion para que funcionen las teclas 1,2,3
+		pantalla.setFocusable(true);
+		pantalla.requestFocusInWindow();
+		//Configuracion del Frame principal
+		setTitle("Missile Comand");
+		setSize(525, 525);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		// Timer task que se va a ejecturas cada determinados tics
+		TimerTask task = new TimerTask() {
+			GestorDeNivel juego = GestorDeNivel.getGestorDeNivel();
+			
+			// En el run esta el loop en donde se actualiza y pinta en cada tic.
+			@Override
+			public void run() {
+
+				if ((!juego.Perdio()) && (juego.getNivelActual() != 17)) {
+					// Aca se actualiza todo
+					this.modelar();
+
+					// ACA SE PINTA EL GRAFICO
+					//Graficador.refrescarTopDown(juego.getEstructuras().ActualizarListaDibujables(), 200);
+					this.pintar();
+					
+				}
+				else{
+					juego.terminarJuego();
+					timer.cancel();
+				}
+			}
+			
+			private void pintar() {
+				//pantalla.setOpaque(true);
+				//pantalla.setBackground(java.awt.Color.BLACK);
+				//pantalla.paint(pantalla.getGraphics());
+				repaint();
+			}
+
+			public void modelar() {
+					// ACA SE ACTUALIZA EL MODELO
+					try {
+						juego.modelar();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			}
+		
+		};
+		timer.schedule(task,2,16);
+	}
+	
+}
--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,80 +1,12 @@
 package controlador;
 
+import Ventanas.Juego;
 
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.swing.JFrame;
-
-import modelo.gestores.GestorDeNivel;
-import modelo.gestores.Pantalla;
-
-public class Controlador extends JFrame {
+public class Controlador {
+	public static Juego Juego;
 	public static void main(String args[]) {
-		Controlador controlador = new Controlador();
-		controlador.Controlar();
-		
-
-	}
-
-	public void Controlar() {
-		Timer timer;
-		timer = new Timer();
-		//Variable de el JPanel "pantalla"
-		Pantalla pantalla = new Pantalla();
-		add(pantalla);
-		pantalla.addMouseListener(new AdapterDeMira());
-		
-		//Configuracion del Frame principal
-		setTitle("Missile Comand");
-		setSize(525, 525);
-		setVisible(true);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
-		// Timer task que se va a ejecturas cada determinados tics
-		TimerTask task = new TimerTask() {
-			GestorDeNivel juego = GestorDeNivel.getGestorDeNivel();
-			
-			// En el run esta el loop en donde se actualiza y pinta en cada tic.
-			@Override
-			public void run() {
-
-				if ((!juego.Perdio()) && (juego.getNivelActual() != 17)) {
-					// Aca se actualiza todo
-					this.modelar();
-
-					// ACA SE PINTA EL GRAFICO
-					//Graficador.refrescarTopDown(juego.getEstructuras().ActualizarListaDibujables(), 200);
-					this.pintar();
-					
-				}
-				else{
-					juego.terminarJuego();
-					timer.cancel();
-				}
-			}
-			
-			private void pintar() {
-				//pantalla.setOpaque(true);
-				//pantalla.setBackground(java.awt.Color.BLACK);
-				//pantalla.paint(pantalla.getGraphics());
-				repaint();
-			}
-
-			public void modelar() {
-					// ACA SE ACTUALIZA EL MODELO
-					try {
-						juego.modelar();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-			}
-		
-		};
-		timer.schedule(task,2,16);
+		Juego = new Juego();
+		Juego.Controlar();
 	}
 	
 }
